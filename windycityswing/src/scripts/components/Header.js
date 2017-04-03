8f4d23f16f44b13f8ddf9f3df191992a30f5b7fa
Jeremy Ma <jyaoma@gmail.com>
2017-04-02 20:20:49 -0500

    Removes unnecessary code [#139627827]

--- windycityswing/src/scripts/components/Header.js
+++ windycityswing/src/scripts/components/Header.js
@@ -6,12 +6,6 @@ class Header extends React.Component {
             <div id='header'>
                 <span id='title'>Windy City Swing</span>
                 <div id="subtitle">THE SWING DANCING INFORMATION IN CHICAGO AND CHICAGOLAND AREA</div>
-
-                {/* <div id="main-menu">
-                    <div className="main-menu-item" id="venue-button">Venues</div>
-                    <div className="main-menu-item" id="event-button">Events</div>
-                    <div className="main-menu-item" id="class-button">Classes</div>
-                </div> */}
             </div>
         );
     }
