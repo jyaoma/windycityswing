2a91bb9cf1164e2b350ecbdc8fc7fb8552b8bc3d
Jeremy Ma <jyaoma@gmail.com>
2017-05-30 19:39:33 -0500

    Adds title to event page
    [#144663467]

--- windycityswing/src/scripts/components/event/EventPage.js
+++ windycityswing/src/scripts/components/event/EventPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import dances from '../../../../dances/dances';
+
 const EventPage = ({match}) => {
+    const eventFinder = event => event.className === match.params.eventName;
+    const event = dances.find(eventFinder);
+
     return (
     <div className='event-details'>
-        {match.params.eventName}
+        <span className='event-details__title'>{event.title}</span>
     </div>);
 }
 
