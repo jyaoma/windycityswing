f682d6754d4d7b7487d046085ea514c1f9c3efe8
Jeremy Ma <jyaoma@gmail.com>
2017-06-04 16:53:50 -0500

    Restyles the event page description for mobile devices
    [#144663537]

--- windycityswing/src/scripts/components/event/EventPage.js
+++ windycityswing/src/scripts/components/event/EventPage.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 import dances from '../../../../dances/dances';
 
+const eventDescription = (event) => {
+    if (!event.info || event.info.length === 0) {
+        return null;
+    }
+    const info = event.info;
+    const result = [];
+    for (let i = 0; i < info.length; i++) {
+        result.push(<span key={i} className='event-details__description'>{info[i]}</span>);
+    }
+
+    return result;
+};
+
 const EventPage = ({match}) => {
     const eventFinder = event => event.className === match.params.eventName;
     const event = dances.find(eventFinder);
@@ -9,6 +22,8 @@ const EventPage = ({match}) => {
     return (
     <div className='event-details'>
         <span className='event-details__title'>{event.title}</span>
+        <span className='event-details__description-header'>DESCRIPTION</span>
+        {eventDescription(event)}
     </div>);
 }
 
