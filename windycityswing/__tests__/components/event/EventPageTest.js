307476fdc952f8ead95c0afba25da71557c0a325
Jeremy Ma <jyaoma@gmail.com>
2017-07-30 22:46:44 -0500

    Adds Google Calendar Link to events
    [#144664309]

--- windycityswing/__tests__/components/event/EventPageTest.js
+++ windycityswing/__tests__/components/event/EventPageTest.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
 import EventPage from '..\\..\\..\\src\\scripts\\components\\event\\EventPage';
 
 it('renders correctly', () => {
-    expect(renderer.create(<EventPage match={{params: {eventName: 'fizz'}}}/>).toJSON()).toMatchSnapshot();
+    expect(renderer.create(<EventPage match={{params: {eventName: 'fizz', date: '2017-07-03'}}}/>).toJSON()).toMatchSnapshot();
+});
+
+it('has the correct Google Calendar link', () => {
+    const tree = shallow(<EventPage match={{params: {eventName: 'fizz', date: '2017-07-03'}}}/>);
+
+    const googleCalendarLink = tree.find('.event-details__link--gcal');
+
+    expect(googleCalendarLink.props().href).toEqual(`https://calendar.google.com/calendar/render?action=TEMPLATE&text=Fizz&dates=20170704T010000Z/20170704T050000Z&details=Occurs every Monday.%0D%0AAll ages event.&location=Lion Head Pub, 2251 N Lincoln Ave 2nd Floor,+Chicago,+IL 60614#eventpage_6`);
 });
--- windycityswing/src/scripts/components/event/EventPage.js
+++ windycityswing/src/scripts/components/event/EventPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import dances from '../../../../dances/dances';
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 const eventDescription = (event) => {
     if (!event.info || event.info.length === 0) {
@@ -89,10 +89,7 @@ const eventTimeline = (event) => {
     return result;
 }
 
-const eventLinks = (event) => {
-    if (!event.infoUrl && !event.facebookUrl) {
-        return null;
-    }
+const eventLinks = (event, date) => {
     const results = [];
     results.push(<span key='link' className='event-details__header'>LINKS</span>);
     if (!!event.infoUrl) {
@@ -115,6 +112,45 @@ const eventLinks = (event) => {
             </svg>
         </a>);
     }
+
+    let description = event.info[0];
+
+    for (let i = 1; i < event.info.length; i++) {
+        description += '%0D%0A' + event.info[i];
+    }
+
+    const dateObject = moment(date, 'YYYY-MM-DD').hour(event.schedule['1'].hour).minute(event.schedule['1'].minute).second(0).tz('America/Chicago');
+
+    //
+    const genericStart = event.timezone.startTimestamp;
+    const genericStartYear = parseInt(genericStart.substr(0, 4));
+    const genericStartMonth = parseInt(genericStart.substr(4, 2));
+    const genericStartDay = parseInt(genericStart.substr(6, 2));
+
+    const genericEnd = event.timezone.endTimestamp;
+    const genericEndYear = parseInt(genericEnd.substr(0, 4));
+    const genericEndMonth = parseInt(genericEnd.substr(4, 2));
+    const genericEndDay = parseInt(genericEnd.substr(6, 2));
+
+    const yearDifference = genericEndYear - genericStartYear;
+    const monthDifference = genericEndMonth - genericStartMonth;
+    const dayDifference = genericEndDay - genericStartDay;
+
+    const lastScheduleEvent = event.schedule[String(Object.keys(event.schedule).length)];
+    const endDateObject = moment(dateObject).add(yearDifference, 'years').add(monthDifference, 'months').add(dayDifference, 'days').hour(lastScheduleEvent.hour).minute(lastScheduleEvent.minute).second(0).tz('America/Chicago');
+    //
+
+    description.replace(' ', '+');
+
+    results.push(<a key='gcal' href={`https://calendar.google.com/calendar/render?action=TEMPLATE&text=${event.title}&dates=${dateObject.tz('Greenwich').format('YYYYMMDDTHHmmss')}Z/${endDateObject.tz('Greenwich').format('YYYYMMDDTHHmmss')}Z&details=${description}&location=${event.location.addressName}, ${event.location.addressOne} ${event.location.addressTwo ? event.location.addressTwo : ''},+${event.location.city},+${event.location.state} ${event.location.zip}#eventpage_6`} target='_blank' className='event-details__link event-details__link--gcal'>
+        <span>Google Calendar</span>
+        <svg viewBox='0 0 16 16' stroke='white' fill='transparent' className='event-details__link-icon'>
+            <path d='M8 3 L1 3 L1 15 L13 15 L13 8'/>
+            <path d='M10 1 L15 1 L15 6'/>
+            <line x1='7' y1='9' x2='15' y2='1'/>
+        </svg>
+    </a>);
+
     return results;
 }
 
@@ -155,7 +191,7 @@ const EventPage = ({match}) => {
     <div className='event-details'>
         <span className='event-details__title'>{event.title}</span>
 
-        {eventLinks(event)}
+        {eventLinks(event, match.params.date)}
 
         <span className='event-details__header'>DESCRIPTION</span>
         {eventDescription(event)}
