edcb60d950d6a818f8629e61812e74e5711cbfe2
Jeremy Ma <jyaoma@gmail.com>
2017-08-30 12:07:08 -0500

    Adds more monthly events and one-time events in September
    [#146260967]

--- windycityswing/dances/2017/09/dances.js
+++ windycityswing/dances/2017/09/dances.js
@@ -1,5 +1,9 @@
+import FlatCatsPingTom20170907 from './flat-cats-ping-tom20170907.json';
 import OutcastHackneys20170911 from './outcast-hackneys20170911.json';
+import TrafficJam20170909 from './traffic-jam-20170909.json';
 
 export default [
-    OutcastHackneys20170911
+    FlatCatsPingTom20170907,
+    OutcastHackneys20170911,
+    TrafficJam20170909
 ];
--- windycityswing/dances/monthly/dances.js
+++ windycityswing/dances/monthly/dances.js
@@ -1,11 +1,15 @@
+import BalboaSocial from './balboa-social.json';
 import Bluetopia from './bluetopia.json';
 import FirstFridays from './first-fridays.json';
 import OutcastHackneys from './outcast-hackneys.json';
 import ThirdFridays from './chicago-dance.json';
+import TrafficJam from './traffic-jam.json';
 
 export default [
+    BalboaSocial,
     Bluetopia,
     FirstFridays,
     OutcastHackneys,
-    ThirdFridays
+    ThirdFridays,
+    TrafficJam
 ];
