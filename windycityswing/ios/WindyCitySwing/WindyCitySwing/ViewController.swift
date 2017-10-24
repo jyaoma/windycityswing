5f527fa5c05f562c6a3e1c7b394d226accd07993
Jeremy Ma <jyaoma@gmail.com>
2017-10-24 13:16:11 -0500

    iOS Hello World
    [#151837836]

---
+++ windycityswing/ios/WindyCitySwing/WindyCitySwing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WindyCitySwing
+//
+//  Created by Jeremy Ma on 10/24/17.
+//  Copyright © 2017 dumbdimb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
