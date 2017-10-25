ac780695bca5d9c578bcf4d774cd1c753989de27
Jeremy Ma <jyaoma@gmail.com>
2017-10-25 12:54:33 -0500

    Adds blue background to iOS app
    [#152257356]

--- windycityswing/ios/WindyCitySwing/WindyCitySwingUITests/WindyCitySwingUITests.swift
+++ windycityswing/ios/WindyCitySwing/WindyCitySwingUITests/WindyCitySwingUITests.swift
@@ -29,8 +29,8 @@ class WindyCitySwingUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        XCTAssert(app.staticTexts["Hello World!"].exists)
     }
     
 }
