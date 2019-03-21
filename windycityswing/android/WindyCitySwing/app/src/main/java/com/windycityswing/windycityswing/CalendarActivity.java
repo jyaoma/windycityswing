c759847dfdafb59910a56013ab748b78eec9336c
Jeremy Ma <jyaoma@gmail.com>
2019-03-21 02:29:01 -0500

    Test Crashing

--- windycityswing/android/WindyCitySwing/app/src/main/java/com/windycityswing/windycityswing/CalendarActivity.java
+++ windycityswing/android/WindyCitySwing/app/src/main/java/com/windycityswing/windycityswing/CalendarActivity.java
@@ -4,11 +4,17 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.crashes.CrashesListener;
+import com.microsoft.appcenter.crashes.ingestion.models.ErrorAttachmentLog;
+import com.microsoft.appcenter.crashes.model.ErrorReport;
+
+import java.security.CryptoPrimitive;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -20,6 +26,12 @@ public class CalendarActivity extends AppCompatActivity {
         TextView title = findViewById(R.id.title);
         Typeface titleTypeface = Typeface.createFromAsset(getAssets(), "fonts/DancingScript-Regular.otf");
         title.setTypeface(titleTypeface);
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Crashes.generateTestCrash();
+            }
+        });
 
         TextView subtitle = findViewById(R.id.subtitle);
         Typeface subtitleTypeface = Typeface.createFromAsset(getAssets(), "fonts/Rampung.ttf");
@@ -27,6 +39,39 @@ public class CalendarActivity extends AppCompatActivity {
 
         AppCenter.start(getApplication(), "5d5820f3-7c31-42c3-8d70-13e9fa56f81e",
                 Analytics.class, Crashes.class);
+        CrashesListener listener = new CrashesListener() {
+            @Override
+            public boolean shouldProcess(ErrorReport report) {
+                return true;
+            }
+
+            @Override
+            public boolean shouldAwaitUserConfirmation() {
+                return false;
+            }
+
+            @Override
+            public Iterable<ErrorAttachmentLog> getErrorAttachments(ErrorReport report) {
+                return null;
+            }
+
+            @Override
+            public void onBeforeSending(ErrorReport report) {
+
+            }
+
+            @Override
+            public void onSendingFailed(ErrorReport report, Exception e) {
+
+            }
+
+            @Override
+            public void onSendingSucceeded(ErrorReport report) {
+
+            }
+        };
+
+        Crashes.setListener(listener);
         Analytics.trackEvent("App Startup");
     }
 }
